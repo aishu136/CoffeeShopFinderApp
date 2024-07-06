a419723b54528504a757b002e92205b504bc66c6
aishwarya.ravi <aishwarya.ravi@m2r2.co.in>
2024-07-06 13:01:54 +0530

    initial commit

---
+++ src/main/java/com/example/CoffeeShopFinderApp/CoffeeShopFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoffeeShopFinderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopFinderAppApplication.class, args);
+	}
+
+}
